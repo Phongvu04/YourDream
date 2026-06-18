31291ed3358f52550245329fd0bc7e3c002ce672
Forever <phonggiang46260@gmail.com>
2026-06-18 17:24:58 +0700

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const rateLimit = require('express-rate-limit');
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 
 const User = require('./models/User');
 const Goal = require('./models/Goal');
